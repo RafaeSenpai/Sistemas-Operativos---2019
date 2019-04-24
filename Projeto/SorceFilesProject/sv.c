78d0f312d4c65b4da64bb69b666564e8af228199
DanielVieira1996 <danyvieira_96@hotmail.com>
2019-04-24 21:53:56 +0100

    ola

--- Projeto/SorceFilesProject/sv.c
+++ Projeto/SorceFilesProject/sv.c
@@ -16,6 +16,7 @@ int main(int argc, char const *argv[])
    char *myfifo2 = "/tmp/server_to_client_fifo";
 
    char buf[1024];
+   char str[1024];
 
    /* create the FIFO (named pipe) */
    mkfifo(myfifo, 0666);
@@ -25,33 +26,38 @@ int main(int argc, char const *argv[])
    client_to_server = open(myfifo, O_RDONLY);
    server_to_client = open(myfifo2, O_WRONLY);
 
-   printf("Server ON.\n");
+   write(1,"Server ON.\n",11);
 
    
    while (1)
    {
-      read(client_to_server,buf,1024 ); // LER O QUE O CLIENTE ESCREVE
-
+      int n=read(client_to_server,buf,1024); // LER O QUE O CLIENTE ESCREVE
+      //buf[n-1]='\0';
       if (strcmp("exit",buf)==0)
-      {
-         printf("Server OFF.\n");
+      {  
+         write(1,"Server OFF.\n",10);
          break;
       }
 
-      else if (strcmp("",buf)!=0)
-      {
-         printf("Received: %s\n", buf);
-         printf("Sending back...\n");
-         write(server_to_client,buf,BUFSIZ);
+         
+
+      else if (strcmp(" ",buf)!=0)
+      {  
+         write(1,"Received: \n",10);
+         write(1,buf,n);
+
+         //write(0,"Sending: %s\n", n);
+         write(server_to_client,buf,n);
       }
+         memset(buf, 0, n);
 
        //clean buf from any data 
-      memset(buf, 0, sizeof(buf));
+      //memset(buf, 0, sizeof(buf-1));
    }
 
    
-   //close(client_to_server);
-   //close(server_to_client);
+   close(client_to_server);
+   close(server_to_client);
 
    unlink(myfifo);
    unlink(myfifo2);
