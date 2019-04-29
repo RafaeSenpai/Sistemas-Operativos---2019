d327855bdb4a5ce4971f9a7b51a47821c7406274
Rafael <rafael.alves.proff@gmail.com>
2019-04-29 18:45:15 +0100

    Adicionada nova pasta com a data de 29-04-2019. A mesma contem já codigo de incersão de vendas. Até ao momento quando faço visualizar uma venda no ficheiro Vendas.txt é-me devolvido dados incorretos, daí não saber se o pronlema está na gravação das vendas no ficheiro vendas.txt ou se na consulta das vendas no mesmo ficheiro!

---
+++ Projeto/Temp_Rafael/Manutenção de Artigos/29-04-2019/Com verifDescritores e mallocs/cv.c
@@ -0,0 +1,14 @@
+#include "cvAPI.h"
+
+int main(){
+char buffer[1024];
+
+	/*
+		Instrucao readln fica com o conteudo da linha lida do terminal até encontrar um \n
+	*/
+	while(cvReadln(0,buffer,1024)){
+		cvMenuComandos(buffer);
+	}
+
+return 0;
+}
