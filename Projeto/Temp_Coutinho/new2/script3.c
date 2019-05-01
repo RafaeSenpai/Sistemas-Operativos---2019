91ba1766159ff78257220acf27b09520f9c86e82
joaocoutinho1602 <a86272@alunos.uminho.pt>
2019-05-01 19:23:22 +0100

    scripts finais

---
+++ Projeto/Temp_Coutinho/new2/script3.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#import <math.h>
+
+int main(){
+
+  int i; float x;
+  int fd = open("script_p.txt", O_RDWR | O_CREAT, 0777);
+
+  char* insercao = NULL;
+
+  for(i = 0; i < 75000; i++){
+    insercao = malloc(50*sizeof(char));
+    x = i * sqrt(3);
+    sprintf(insercao, "p %d %f\n", i, x);
+    write(fd, insercao, strlen(insercao));
+    free(insercao);
+  }
+  close(fd);
+  return 0;
+}
